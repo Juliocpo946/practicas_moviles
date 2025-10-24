b6fc1b446ca448e69fc5b02ac2b2326391831a95
Juliocpo946 <juliocesarp6273@gmail.com>
2025-10-24 15:10:33 -0600

    implementacion de rutas v1

---
+++ lib/core/route/app_router.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/cupertino.dart';
+
+class AppRouter{
+  static Route<dynamic>? onGenerateroute(RouteSettings settings){
+    switch (settings.name){
+      case AppRoutes.home:
+
+    }
+  }
+}
